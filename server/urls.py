461a8a077c068e73f203a515cd6756795123ff84
Shadrack S <shadrackshibu1@yahoo.com>
2025-08-19 10:50:42 +0000

    update code

---
+++ server/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from server import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('drinks/', views.drink_list ),
+    path('drinks/<int:pk>/', views.drink_detail),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
